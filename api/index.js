243cbaa12d3d0de50ed0f9f4002985e6e5c5a953
o0001 <fernandesfrancisco@ipvc.pt>
2025-05-29 22:58:18 +0100

    fix permissions

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // api/index.js
-const express    = require('express');
+const express = require('express');
 const serverless = require('serverless-http');
-const cors       = require('cors');
+const cors = require('cors');
 require('dotenv').config();
 
 const usersRouter    = require('./users');
@@ -10,14 +10,24 @@ const recipesRouter  = require('./recipes');
 const productsRouter = require('./products');
 
 const app = express();
-app.use(cors());
+
+// 1) CORS configuration
+const corsOptions = {
+  origin: '*', // or specify ['http://localhost:8100'] for dev
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // preflight
+
 app.use(express.json());
 
-// Mount all routes under /v1 instead of /api
+// 2) Mount routers under /v1
 app.use('/v1/users',    usersRouter);
 app.use('/v1/foods',    foodsRouter);
 app.use('/v1/recipes',  recipesRouter);
 app.use('/v1/products', productsRouter);
 
-module.exports      = app;
+module.exports = app;
 module.exports.handler = serverless(app);
