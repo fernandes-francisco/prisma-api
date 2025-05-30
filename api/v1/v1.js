0d29146c9fb4ed018937e4d3e7590e4dc952c5b8
o0001 <fernandesfrancisco@ipvc.pt>
2025-05-30 14:27:23 +0100

    fix v19

--- api/v1/v1.js
+++ api/v1/v1.js
@@ -1,20 +1,23 @@
+// api/v1/[...slug].js
 const express    = require('express');
 const serverless = require('serverless-http');
 const cors       = require('cors');
 require('dotenv').config();
 
-const usersRouter    = require('./users');
-const foodsRouter    = require('./foods');
-const recipesRouter  = require('./recipes');
-const productsRouter = require('./products');
+const usersRouter    = require('../users');
+const foodsRouter    = require('../foods');
+const recipesRouter  = require('../recipes');
+const productsRouter = require('../products');
 
 const app = express();
 
-// Allow CORS from your Ionic dev origin 
+// CORS
 app.use(cors({ origin: ['http://localhost:8100', '*'] }));
+app.options('*', cors());
+
 app.use(express.json());
 
-// Mount sub‐routes at /api/v1/*
+// Mount your routes under /api/v1
 app.use('/api/v1/users',    usersRouter);
 app.use('/api/v1/foods',    foodsRouter);
 app.use('/api/v1/recipes',  recipesRouter);
